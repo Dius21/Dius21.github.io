b8ec2e764ba3abdb061a3099b3b33a8e7425364b
Dius21 <virajdhara21@gmail.com>
2023-01-19 09:35:58 +0530

    Version 0.1

---
+++ scripts.js
@@ -0,0 +1,4 @@
+today=new Date();
+yyyy = today.getFullYear();
+var age = yyyy -2006;
+document.getElementById("date").innerHTML = age+" years ago I was born in Mumbai, lived for bunch of years in my first house, then shifted before I was 10, at my new place I was intrigued by our computer and I mostly grew up in my second school, LFS, then took the most common: math science, CS for optional subjects and loved all so here I am giving one of the toughest competitive exams in the world in hopes of getting into some decent Collage."
